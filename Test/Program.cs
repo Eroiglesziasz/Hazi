35fee9a2e0402a8360ae8e6c76c6054d9ea4add2
Eroice <39998814+Eroiglesziasz@users.noreply.github.com>
2022-09-23 22:31:27 +0200

    3.0

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,13 @@
 Console.WriteLine("Adj meg két számot");
-int szam1 = int.Parse(Console.ReadLine());
-int szam2 = int.Parse(Console.ReadLine());
-Console.ReadLine();
+Console.Write("Első szám: "); int szam1 = int.Parse(Console.ReadLine());
+Console.Write("Második szám: "); int szam2 = int.Parse(Console.ReadLine());
+
+if (szam1 > szam2)
+{
+    Console.WriteLine(szam1);
+}
+else
+{
+    Console.WriteLine(szam2);
+}
+Console.ReadKey();
